c4f065f41cacc9e80ea9b15decbfc71b136819c5
JesAje <jesustecnologia8@gmail.com>
2025-10-08 23:14:57 -0300

    Update urls.py

--- evaluation/urls.py
+++ evaluation/urls.py
@@ -1,1 +1,10 @@
+# evaluation/urls.py
+from django.urls import path
+from .views import peer_review_view
+
+app_name = "evaluation"
+
+urlpatterns = [
+    path("<int:session_id>/peer-review/", peer_review_view, name="peer_review"),
+]
 
