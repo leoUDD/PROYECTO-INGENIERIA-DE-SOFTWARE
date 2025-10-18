4c3eb1b99d18d21461e7de90682e0df62542958d
Seba-RC <sebarc2005@gmail.com>
2025-10-18 15:50:49 -0300

    Reapply "+ models desde BDD"
    This reverts commit bdcd8476e2e32560afb77f17898a82eb5595017f.

--- config/settings.py
+++ config/settings.py
@@ -15,6 +15,17 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'misionemprende',
+            'USER': 'root',
+            'PASSWORD': 'pass123',
+            'HOST': 'localhost',  # or the database server's IP/hostname
+            'PORT': '3306',      # default MySQL port
+        }
+    }
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -74,12 +85,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
